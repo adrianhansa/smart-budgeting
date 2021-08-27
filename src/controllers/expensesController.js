ce81a355bdfc571899a838a9fdd06780e1d35028
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-27 16:50:16 +0100

    Created getExpense, updateExpense and deleteExpense functions.

--- src/controllers/expensesController.js
+++ src/controllers/expensesController.js
@@ -17,8 +17,9 @@ const createExpense = async (req,res)=>{
 
 const getExpense = async (req,res)=>{
     try{
-        const expenses = await Expense.find({user:req.user})
-        res.status(200).json(expenses)
+        const expense = await Expense.findById(req.params.id).populate('account','name')
+        if(!expense) return res.status(404).json({message:"Expense not found"})
+        res.status(200).json(expense)
     }catch(error){
         return res.status(500).json({message:error.message})
     }
@@ -26,7 +27,7 @@ const getExpense = async (req,res)=>{
 
 const getExpenses = async (req,res)=>{
     try{
-        const expenses = await Expense.find({user:req.user})
+        const expenses = await Expense.find({user:req.user}).populate('account','name')
         res.status(200).json(expenses)
     }catch(error){
         return res.status(500).json({message:error.message})
@@ -35,7 +36,10 @@ const getExpenses = async (req,res)=>{
 
 const updateExpense = async (req,res)=>{
     try{
-        //
+        const {amount,description,date,account} = req.body
+        if(!amount || !description || !date || !account) return res.status(400).json({message:"Please complete all fields."})
+        const expense = await Expense.findByIdAndUpdate(req.params.id,{amount:Number(amount),description,date,account},{new:true})
+        res.status(200).json(expense)
     }catch(error){
         return res.status(500).json({message:error.message})
     }
@@ -43,7 +47,9 @@ const updateExpense = async (req,res)=>{
 
 const deleteExpense = async (req,res)=>{
     try{
-        //
+        const expense = await Expense.findByIdAndDelete(req.params.id)
+        if(!expense) return res.status(404).json({message:"Expense not found"})
+        res.status(200).json({message:"Expense deleted."})
     }catch(error){
         return res.status(500).json({message:error.message})
     }
