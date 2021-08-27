53d066daf3526ff8a4f2313100f2e3142cd53948
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-27 11:07:50 +0100

    Created update, get and delete account endpoints; updated account routes.

--- src/controllers/accountsController.js
+++ src/controllers/accountsController.js
@@ -24,4 +24,45 @@ const getAccounts = async (req,res)=>{
     }
 }
 
-module.exports = {createAccount,getAccounts}
+
+const getAccount = async (req,res)=>{
+    try{
+        const account = await Account.findOne({user:req.user,slug:req.params.slug})
+        if(!account) return res.status(404).json({message: "Account not found."})
+        res.status(200).json(account)
+    }catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+const updateAccount = async (req,res)=>{
+    try{
+        const {name} = req.body
+        if(!name) return res.status(400).json({name:"Name must be provided."})
+        //check if new name is different from existing one providing the initial slug
+        console.log(req.user)
+        console.log(req.params.slug)
+        const existingAccount = await Account.findOne({user:req.user,slug:req.params.slug})
+        if(existingAccount.name === name) return res.status(200).json({account:existingAccount})
+        const newSlug = slugify(name, {lower:true,remove: /[*+~.()'"!?:@]/g})
+        //check if the new name exists in the database for this user
+        const existingName = await Account.findOne({user:req.user,slug:newSlug})
+        if(existingName) return res.status(400).json({message:"You have already created an account with this name."})
+        const account = await Account.findOneAndUpdate({user:req.user,slug:existingAccount.slug},{name,slug:newSlug},{new:true})
+        res.status(200).json(account)
+    }catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+const deleteAccount = async (req,res)=>{
+    try{
+        const account = await Account.findOneAndDelete({user:req.user,slug:req.params.slug})
+        if(!account) return res.status(404).json({message:"Account not found."})
+        res.status(200).json({message:"Account deleted successfully."})
+    }catch(error){
+        return res.status(500).json({error:error.message})
+    }
+}
+
+module.exports = {createAccount,getAccounts,getAccount,updateAccount,deleteAccount}
--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
-const {createAccount,getAccounts} = require('../controllers/accountsController')
+const {createAccount,getAccounts,getAccount,updateAccount,deleteAccount} = require('../controllers/accountsController')
 const auth = require('../middlewares/auth')
 
 router.post('/', auth, createAccount)
 router.get('/', auth, getAccounts)
+router.get("/:slug",auth, getAccount)
+router.put("/:slug",auth, updateAccount)
+router.delete("/:slug",auth, deleteAccount)
 
 module.exports = router
