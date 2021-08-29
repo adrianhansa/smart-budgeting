a7f66da1f68f4cf7631ecb1036782f16248dae0e
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-29 09:29:17 +0100

    Added localhost:19006 to thr cors origin list

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv/config')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors({origin:['http://localhost:3000']}))
+app.use(express.json());
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:19006/"] }));
 
-const userRoutes = require('./src/routes/userRoutes')
-app.use('/',userRoutes)
+const userRoutes = require("./src/routes/userRoutes");
+app.use("/", userRoutes);
 
-const accountRoutes = require('./src/routes/accountRoutes')
-app.use("/accounts", accountRoutes)
+const accountRoutes = require("./src/routes/accountRoutes");
+app.use("/accounts", accountRoutes);
 
-const expenseRoutes = require('./src/routes/expenseRoutes')
-app.use('/expenses', expenseRoutes)
+const expenseRoutes = require("./src/routes/expenseRoutes");
+app.use("/expenses", expenseRoutes);
 
-const budgetLimits = require('./src/routes/budgetLimitsRoutes')
-app.use('/budget-limits',budgetLimits)
+const budgetLimits = require("./src/routes/budgetLimitsRoutes");
+app.use("/budget-limits", budgetLimits);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DB_CONNECTION).then(()=>{
-    console.log("Connected to mongodb Atlas")
-    app.listen(PORT, ()=>console.log(`Server listening on port ${PORT}.`))
-})
+mongoose.connect(process.env.DB_CONNECTION).then(() => {
+  console.log("Connected to mongodb Atlas");
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+});
