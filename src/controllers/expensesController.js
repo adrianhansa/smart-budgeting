ae8155dc8a4435bf180349fec53fce8a777f040a
Adrian Hansa <adrian.hansa@yahoo.com>
2021-12-11 11:10:14 +0000

    Included household in Expense model; update expense related methods to reflect this.

--- src/controllers/expensesController.js
+++ src/controllers/expensesController.js
@@ -1,81 +1,142 @@
-const Expense = require('../models/Expense')
+const Expense = require("../models/Expense");
 
-const createExpense = async (req,res)=>{
-    try{
-        const amount = Number(req.body.amount)
-        if(!amount) return res.status(400).json({message:"Amount spent is required."})
-        const account = req.body.account
-        if(!account) return res.status(400).json({message:"Please select an account."})
-        const description = req.body.description
-        if(!description) return res.status(400).json({message:"Please enter a description."})
-        const expense = await Expense.create({user:req.user,account,amount,description,date:Date.now(),month:new Date().getMonth()+1,year:new Date().getFullYear()})
-        res.status(200).json(expense)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const createExpense = async (req, res) => {
+  try {
+    const amount = Number(req.body.amount);
+    if (amount < 0)
+      return res
+        .status(400)
+        .json({ message: "The amount spent cannot be less than zero." });
+    if (!amount)
+      return res.status(400).json({ message: "Amount spent is required." });
+    const account = req.body.account;
+    if (!account)
+      return res.status(400).json({ message: "Please select an account." });
+    const description = req.body.description;
+    if (!description)
+      return res.status(400).json({ message: "Please enter a description." });
+    const date = req.body.date;
+    if (!date)
+      return res.status(400).json({ message: "Please select a date." });
+    const expense = await Expense.create({
+      user: req.user._id,
+      household: req.user.household._id,
+      account,
+      amount,
+      description,
+      date,
+      month: date.getMonth() + 1,
+      year: date.getFullYear(),
+    });
+    res.status(200).json(expense);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-const getExpense = async (req,res)=>{
-    try{
-        const expense = await Expense.findById(req.params.id).populate('account','name')
-        if(!expense) return res.status(404).json({message:"Expense not found"})
-        res.status(200).json(expense)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const getExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id).populate(
+      "account",
+      "name"
+    );
+    if (!expense) return res.status(404).json({ message: "Expense not found" });
+    res.status(200).json(expense);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-const getExpenses = async (req,res)=>{
-    try{
-        const expenses = await Expense.find({user:req.user}).populate('account','name')
-        res.status(200).json(expenses)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const getExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find({ household: req.user.household._id })
+      .populate("account", "name")
+      .populate("user", "name");
+    res.status(200).json(expenses);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-const getExpensesByMonthAndYear = async (req,res)=>{
-    try{
-        const {month, year} = req.params
-        if(!month || !year) return res.status(400).json({message:"Please select both the month and the year."})
-        const expenses = await Expense.find({user:req.user}).where('year').equals(year).where('month').equals(month).populate('account','name')
-        res.status(200).json(expenses)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const getExpensesByMonthAndYear = async (req, res) => {
+  try {
+    const month = Number(req.params.month);
+    const year = Number(req.params.year);
+    if (!month || !year)
+      return res
+        .status(400)
+        .json({ message: "Please select both the month and the year." });
+    const expenses = await Expense.find({ household: req.user.household._id })
+      .where("year")
+      .equals(year)
+      .where("month")
+      .equals(month)
+      .populate("account", "name")
+      .poupulate("user", "name");
+    res.status(200).json(expenses);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-const getExpensesByAccount = async (req,res)=>{
-    try{
-        const {account} = req.body
-        if(!account) return res.status(400).json({message:"Please select the account."})
-        const expenses = await Expense.find({user:req.user,account})
-        res.status(200).json(expenses)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const getExpensesByAccount = async (req, res) => {
+  try {
+    if (!req.params.account)
+      return res.status(400).json({ message: "Please select the account." });
+    const expenses = await Expense.find({
+      household: req.user.household._id,
+      account: req.params.account,
+    });
+    res.status(200).json(expenses);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-const updateExpense = async (req,res)=>{
-    try{
-        const {amount,description,date,account} = req.body
-        //include date in the if below
-        if(!amount || !description || !account) return res.status(400).json({message:"Please complete all fields."})
-        const expense = await Expense.findByIdAndUpdate(req.params.id,{amount:Number(amount),description,date:Date.now(),account,month:new Date().getMonth()+1,year:new Date().getFullYear()},{new:true})
-        res.status(200).json(expense)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const updateExpense = async (req, res) => {
+  try {
+    const { amount, description, date, account } = req.body;
+    //include date in the if below
+    if (!amount || !description || !account || !date)
+      return res.status(400).json({ message: "Please complete all fields." });
+    if (amount < 0)
+      return res
+        .status(400)
+        .json({ message: "The amount spent cannot be less than zero." });
+    const expense = await Expense.findByIdAndUpdate(
+      req.params.id,
+      {
+        amount: Number(amount),
+        description,
+        date: date,
+        account,
+        month: date.getMonth() + 1,
+        year: date.getFullYear(),
+      },
+      { new: true }
+    );
+    res.status(200).json(expense);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-const deleteExpense = async (req,res)=>{
-    try{
-        const expense = await Expense.findByIdAndDelete(req.params.id)
-        if(!expense) return res.status(404).json({message:"Expense not found"})
-        res.status(200).json({message:"Expense deleted."})
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-}
+const deleteExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findByIdAndDelete(req.params.id);
+    if (!expense) return res.status(404).json({ message: "Expense not found" });
+    res.status(200).json({ message: "Expense deleted." });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-module.exports = {createExpense, updateExpense, deleteExpense, getExpense, getExpenses, getExpensesByAccount, getExpensesByMonthAndYear}
+module.exports = {
+  createExpense,
+  updateExpense,
+  deleteExpense,
+  getExpense,
+  getExpenses,
+  getExpensesByAccount,
+  getExpensesByMonthAndYear,
+};
--- src/models/Expense.js
+++ src/models/Expense.js
@@ -1,13 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const expenseSchema = new mongoose.Schema({
-    user: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'users'},
-    account: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'accounts'},
-    amount: {type: Number, required: true},
-    description:{type: String, required: true},
-    date: {type: Date, required: true},
-    month: {type: String, required: true},
-    year: {type: String, required: true}
-})
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "users" },
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "accounts",
+  },
+  household: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "households",
+  },
+  amount: { type: Number, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  month: { type: Number, required: true },
+  year: { type: Number, required: true },
+});
 
-module.exports = mongoose.model('expenses', expenseSchema)
+module.exports = mongoose.model("expenses", expenseSchema);
