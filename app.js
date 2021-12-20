143d5d347271477828c4eadfef70c35f122550c4
Adrian Hansa <adrian.hansa@yahoo.com>
2021-12-20 20:46:35 +0000

    Listening to and broadcasting events upon creating, updating and deleting incomes and expenses.

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const mongoose = require("mongoose");
-require("dotenv/config");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-app.use(cookieParser());
-app.use(express.json());
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5000/"],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("expense-created", (data) =>
+    socket.broadcast.emit("expense-created", data)
+  );
+  socket.on("income-created", (data) =>
+    socket.broadcast.emit("income-created", data)
+  );
+  socket.on("expense-updated", (data) =>
+    socket.broadcast.emit("expense-updated", data)
+  );
+  socket.on("income-updated", (data) =>
+    socket.broadcast.emit("income-updated", data)
+  );
+  socket.on("expense-deleted", (data) =>
+    socket.broadcast.emit("expense-deleted", data)
+  );
+  socket.on("income-deleted", (data) =>
+    socket.broadcast.emit("income-deleted", data)
+  );
+});
+
+const mongoose = require("mongoose");
+require("dotenv/config");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+app.use(express.json());
+
 const userRoutes = require("./src/routes/userRoutes");
-app.use("/", userRoutes);
-
 const accountRoutes = require("./src/routes/accountRoutes");
-app.use("/accounts", accountRoutes);
-
 const expenseRoutes = require("./src/routes/expenseRoutes");
 const incomeRoutes = require("./src/routes/incomeRoutes");
+app.use("/", userRoutes);
+app.use("/accounts", accountRoutes);
 app.use("/expenses", expenseRoutes);
 app.use("/income", incomeRoutes);
 
@@ -32,8 +62,4 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.DB_CONNECTION).then(() => {
   console.log("Connected to mongodb Atlas");
   server.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
-});
-
-io.on("connect", (socket) => {
-  console.log(socket.id);
 });
