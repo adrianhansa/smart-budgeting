8d3401d63a76ae0553050378b25f5ddb55dfbadd
Adrian Hansa <adrian.hansa@yahoo.com>
2021-12-31 16:59:39 +0000

    Added app's heroku url to the cors origin

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const Event = require("./src/models/Event");
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://smart-budgeting.herokuapp.com"],
     credentials: true,
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://smart-budgeting.herokuapp.com"],
     credentials: true,
   })
 );
