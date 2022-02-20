fe693523f4e0431d10267cd72959d90db8840084
Adrian Hansa <adrian.hansa@yahoo.com>
2022-02-20 19:47:10 +0000

    Removed websockets and events module

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const Event = require("./src/models/Event");
-
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000", "https://ebudgeting.netlify.app"],
-    credentials: true,
-  },
-});
 
 app.use(
   cors({
@@ -19,72 +8,6 @@ app.use(
     credentials: true,
   })
 );
-
-io.on("connection", (socket) => {
-  socket.on("expense-created", async (data) => {
-    await Event.create({
-      date: new Date(),
-      user: data.user.id,
-      household: data.user.household._id,
-      description: `${data.user.name} recorded an expense of ${data.amount} as ${data.description} purchsed on ${data.date}.`,
-    });
-    socket.emit("expense-created", data);
-    socket.emit("event-created");
-  });
-  socket.on("income-created", async (data) => {
-    await Event.create({
-      date: new Date(),
-      user: data.user.id,
-      household: data.user.household._id,
-      description: `${data.user.name} recorded the income of ${data.amount} as ${data.description} earned on ${data.date}.`,
-    });
-    socket.emit("income-created", data);
-    socket.emit("event-created");
-  });
-  socket.on("expense-updated", async (data) => {
-    socket.emit("expense-updated", data);
-    await Event.create({
-      date: new Date(),
-      user: data.user.id,
-      household: data.user.household._id,
-      description: `${data.user.name} updated an expense of ${data.amount} as ${data.description} purchsed on ${data.date}.`,
-    });
-    socket.emit("event-created");
-  });
-  socket.on("income-updated", async (data) => {
-    socket.emit("income-updated", data);
-    await Event.create({
-      date: new Date(),
-      user: data.user.id,
-      household: data.user.household._id,
-      description: `${data.user.name} updated the income of ${data.amount} as ${data.description} earned on ${data.date}.`,
-    });
-    socket.emit("event-created");
-  });
-  socket.on("expense-deleted", async (data) => {
-    socket.emit("expense-deleted", data);
-    await Event.create({
-      date: new Date(),
-      user: data.user.id,
-      household: data.user.household._id,
-      description: `${data.user.name} deleted an expense of ${data.amount} as ${data.description} spent on ${data.date}.`,
-    });
-    socket.emit("event-created");
-  });
-  socket.on("event-created", () => {
-    socket.emit("event-created");
-  });
-  socket.on("income-deleted", async (data) => {
-    socket.emit("income-deleted", data);
-    await Event.create({
-      date: new Date(),
-      user: data.user.id,
-      household: data.user.household._id,
-      description: `${data.user.name} deleted an income of ${data.amount} as ${data.description} earned on ${data.date}.`,
-    });
-    socket.emit("event-created");
-  });
-});
 
 const mongoose = require("mongoose");
 require("dotenv/config");
@@ -110,5 +33,5 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_CONNECTION).then(() => {
   console.log("Connected to mongodb Atlas");
-  server.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
 });
